825cf321ecef364100dcca5afe54443ea5305082
navjeetpatil <navjeetpatil03@gmail.com>
2025-06-05 20:40:30 +0530

    Hello World 6 New Line Added

--- src/Ex_01_Java_Basic/Lab00001_HelloWorld.java
+++ src/Ex_01_Java_Basic/Lab00001_HelloWorld.java
@@ -3,5 +3,6 @@ package Ex_01_Java_Basic;
 public class Lab00001_HelloWorld {
     public static void main(String [] args){
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
