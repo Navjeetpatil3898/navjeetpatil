86109dd29b21193c0022c8685bb1eca817baa2c8
navjeetpatil <navjeetpatil03@gmail.com>
2025-06-26 08:16:11 +0530

    Coding Question in tne MCQ Test

---
+++ src/Ex_01_Java_Basic/String_MCQ_Exam.java
@@ -0,0 +1,11 @@
+package Ex_01_Java_Basic;
+
+public class String_MCQ_Exam {
+    public static void main(String[] args){
+        String str = "Hello World";
+
+        System.out.println("Length Is:"+str.length());
+        System.out.println("First char Is:"+str.charAt(0));
+        System.out.println("Substring Is:"+str.substring(5));
+    }
+}
